231ac614d44a5c943556b6d8599f605a6a9e3553
riooktapian29@gmail.com <49826587+riooktapian@users.noreply.github.com>
2019-10-17 10:31:40 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_2320822341581370";
+$raw_uid="e51c994d-6337-45f4-819a-5e262aa90a83";
+$user_name="Ryo+Oktavyan";
+$invite_code="FAd1NU6Kgk";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="973b0f3284dc8494";
+$device_type="Xiaomi++Redmi+3S";
+
+
+?>
